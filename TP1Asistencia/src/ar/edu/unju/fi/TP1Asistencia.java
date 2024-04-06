3d90fbeb2f50b71c7ba0f50887ce34230b3ace84
User <User@DESKTOP-VA9RCLK>
2024-04-05 21:54:47 -0300

    se agrega paquete y clase para el ejercico 1

---
+++ TP1Asistencia/src/ar/edu/unju/fi/TP1Asistencia.java
@@ -0,0 +1,17 @@
+package ar.edu.unju.fi;
+
+public class TP1Asistencia {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n1= 8;
+		int n2=5;
+		int n3=1;
+		int n4=20;
+		int n5=9;
+		float promedio = (n1 + n2 + n3 + n4 + n5 )/(5);
+		System.out.println("el promedio es: " + promedio ); 		
+	
+	}
+
+}
