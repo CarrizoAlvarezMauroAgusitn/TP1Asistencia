9a25d87e3ebe2f8aaff8112f2fce266cdcd04fdc
User <User@DESKTOP-VA9RCLK>
2024-04-07 20:00:11 -0300

    se tenia que poner 5.0 para que no redonde el resultado del producto

--- TP1Asistencia/src/ar/edu/unju/fi/TP1Asistencia.java
+++ TP1Asistencia/src/ar/edu/unju/fi/TP1Asistencia.java
@@ -4,12 +4,12 @@ public class TP1Asistencia {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int n1= 8;
-		int n2=5;
-		int n3=1;
-		int n4=20;
-		int n5=9;
-		float promedio = (n1 + n2 + n3 + n4 + n5 )/(5);
+		byte n1= 8;
+		byte n2=5;
+		byte n3=1;
+		byte n4=20;
+		byte n5=9;
+		double promedio = (n1 + n2 + n3 + n4 + n5 )/5.0;
 		System.out.println("el promedio es: " + promedio ); 		
 	
 	}
